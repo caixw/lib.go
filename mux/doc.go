e3cd3572462eff31d37e7c1cf86c9e97813467f0
caixw <xnotepad@gmail.com>
2014-09-22 19:40:10 +0800

    添加mux.matche对象，修改部分文档

--- mux/doc.go
+++ mux/doc.go
@@ -2,5 +2,31 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-// mux包是对http.ServeMux的扩展，实现了很多实用的http.Handler扩展。
+// mux提供了一系列http.Handler接口的实现，方便用户进行正则路由匹
+// 配(Path)、多域名匹配(Host)、根据请求方法匹配(Method)等操作。
+//
+// 一个匹配多域名路由的示例：
+//  var h1, h2, h3, h4 http.Handler
+//
+//  // 声明一个带method匹配的实例
+//  m1 := mux.NewMethod().
+//            Get(mux.NewPath(h1, "api/logout")).
+//            Post(mux.NewPath(h2, "api/login"))
+//
+//  // net/http包里的默认ServeMux实例
+//  srv := http.NewServeMux()
+//  srv.Handle(h3, "/")
+//
+//  // 将srv和一个正则路由的匹配都压入到m2中
+//  m2 := mux.NewMethod().
+//            Get(mux.NewPath(h4, "/")).
+//            Any(mux.Handler2Matcher(srv))
+//
+//  // 将Handler添加到各自的域名下
+//  h1 := mux.NewHost(m1, "api.example.com")
+//  h2 := mux.NewHost(m2, "(\\w+).example.com")
+//
+//  http.ListenAndServe("8080", NewMatches(h1, h2))
 package mux
+
+const Version = "0.1.5.140922"
